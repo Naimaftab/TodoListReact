502da7580d41bb3cf45b6ce62f7f8170fd16503e
Naimaftab <aftab.naima@outlook.fr>
2023-09-12 17:22:01 +0200

    addTodo modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  
+  function addTodo(content){
+
+    const todo = {
+      id: crypto.randomUUID(), //Api Web permet de nous donner une id unique
+      content,
+      done: false, /*todo pas réalisé*/
+      edit: false /*todo pas modifiable*/
+    };
+    setTodoList([...todoList, todo]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center align-items-center p-20">
+      <div className="card container p-20">
+        <h1 className="mb-20">Todo List</h1>
+        <AddTodo addTodo = { addTodo }/>
+        <TodoList />
+      </div>
     </div>
   );
 }
