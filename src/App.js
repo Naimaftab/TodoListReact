91f77bf40e452dd4a1d3ebe07e499f54f3bf421f
Naimaftab <aftab.naima@outlook.fr>
2023-09-13 11:43:51 +0200

    delete todolist with button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
-import { useState } from 'react';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  
-  function addTodo(content){
 
-    const todo = {
-      id: crypto.randomUUID(), //Api Web permet de nous donner une id unique
-      content,
-      done: false, /*todo pas réalisé*/
-      edit: false /*todo pas modifiable*/
-    };
+  function addTodo(content) {
+    const todo = { id: crypto.randomUUID(), done: false, edit: false, content };
     setTodoList([...todoList, todo]);
   }
+
+  function deleteTodo(id) {
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+  } //la méthode filter() retourne un nouveau tableau sans modifier le tableau dans la variable d'état todoList
+
   return (
     <div className="d-flex justify-content-center align-items-center p-20">
       <div className="card container p-20">
-        <h1 className="mb-20">Todo List</h1>
-        <AddTodo addTodo = { addTodo }/>
-        <TodoList />
+        <h1 className="mb-20">Liste de tâches</h1>
+        <AddTodo addTodo={addTodo} />
+        <TodoList todoList={todoList} deleteTodo={deleteTodo} />
       </div>
     </div>
   );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,12 @@
-function TodoItem(){
-    return <h2>Todo Item</h2>
-}
-
-export default TodoItem;
+export default function TodoItem({ todo, deleteTodo }) {
+    return (
+      <li className="mb-10 d-flex justify-content-center align-items-center p-10">
+        <span className="flex-fill">{todo.content}</span>
+        <button className="btn btn-primary mr-15">Valider</button>
+        <button className="btn btn-primary mr-15">Modifier</button>
+        <button className="btn btn-reverse-primary" onClick={deleteTodo}>
+          Supprimer
+        </button>
+      </li>
+    );
+  }
