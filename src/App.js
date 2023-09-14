49209b114f3c1d30f711e384dda2da37726a56a5
Naimaftab <aftab.naima@outlook.fr>
2023-09-14 11:30:37 +0200

    done and start edit

--- src/App.js
+++ src/App.js
@@ -14,12 +14,30 @@ function App() {
     setTodoList(todoList.filter((todo) => todo.id !== id));
   } //la méthode filter() retourne un nouveau tableau sans modifier le tableau dans la variable d'état todoList
 
+  function toggleTodo(id){
+    setTodoList(todoList.map( todo => todo.id === id ? ({
+      ...todo,
+      done: !todo.done
+    }) : todo))
+  }
+
+  function toggleTodoEdit(id){
+    setTodoList(todoList.map( todo => todo.id === id ? ({
+      ...todo,
+      edit: !todo.edit
+    }) : todo))
+  }
   return (
     <div className="d-flex justify-content-center align-items-center p-20">
       <div className="card container p-20">
         <h1 className="mb-20">Liste de tâches</h1>
         <AddTodo addTodo={addTodo} />
-        <TodoList todoList={todoList} deleteTodo={deleteTodo} />
+        <TodoList 
+        todoList={todoList} 
+        deleteTodo={deleteTodo} 
+        toggleTodo={ toggleTodo }
+        toggleTodoEdit = {toggleTodoEdit}
+        />
       </div>
     </div>
   );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,9 @@
-export default function TodoItem({ todo, deleteTodo }) {
+export default function TodoItem({ todo, deleteTodo, toggleTodo, editTodo}) {
     return (
       <li className="mb-10 d-flex justify-content-center align-items-center p-10">
-        <span className="flex-fill">{todo.content}</span>
-        <button className="btn btn-primary mr-15">Valider</button>
-        <button className="btn btn-primary mr-15">Modifier</button>
+        <span className="flex-fill">{todo.content} { todo.done && /* le && sert a faire un toggle */ "( ✓ )"} </span>
+        <button onClick={ toggleTodo } className="btn btn-primary mr-15">Valider</button>
+        <button onClick={ editTodo } className="btn btn-primary mr-15">Modifier</button>
         <button className="btn btn-reverse-primary" onClick={deleteTodo}>
           Supprimer
         </button>
